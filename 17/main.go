cc8ef3ce657f11c2eece5c195a368ad8162d558e
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-26 19:16:30 +0300

    :zap:Complete 17

--- 17/main.go
+++ 17/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+var (
+	v1 = 1
+	v2 = "oleg"
+	v3 = true
+	v4 = make(chan int)
+	v5 = 1.0
+)
+
+func defineType(variable interface{}) {
+	switch variable.(type) {
+	case int:
+		fmt.Printf("%v is int\n", variable)
+	case string:
+		fmt.Printf("%v is string\n", variable)
+	case bool:
+		fmt.Printf("%v is bool\n", variable)
+	case chan int:
+		fmt.Printf("%v is chan int\n", variable)
+	default:
+		fmt.Println("Unknown type")
+	}
+}
+
+func main() {
+	defineType(v1)
+	defineType(v2)
+	defineType(v3)
+	defineType(v4)
+	defineType(v5)
 }
