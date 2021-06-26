c610fffa022cfa93f69c8dc78bf634ec8cba1b60
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-26 19:07:04 +0300

    :zap:Complete 16, 18

--- 16/main.go
+++ 16/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	n := 0
+	if true {
+		n := 1 //из-за := это локальная n для оператора if, которая будет жить внутри {}. Чтоб вывести 2, нужно сделать просто =
+		n++
+	}
+	fmt.Println(n)
 }
--- 18/main.go
+++ 18/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func someAction(v []int8, b int8) {
+	v[0] = 100       //меняет значение в ячейке с индексом 0
+	v = append(v, b) //добавляем новый элемент в слайс, тем самым создаем новый локальный массив, на который указывает слайс
+
+}
+func main() {
+	var a = []int8{1, 2, 3, 4, 5}
+	someAction(a, 6)
+	fmt.Println(a)
 }
