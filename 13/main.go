32fa6feeb15932aad696eb20f7443f5cbe9400b2
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-26 18:57:47 +0300

    :zap:Complete 12, 13, 15

--- 13/main.go
+++ 13/main.go
@@ -11,9 +11,18 @@ func main() {
 		wg.Add(1)
 		go func(wg sync.WaitGroup, i int) {
 			fmt.Println(i)
-			wg.Done()
-		}(wg, i)
+			wg.Done() //Done выполняется для копии wg из анонимной функции и никак не влияет на WaitGroup в main
+		}(wg, i) //передаем копию wg
 	}
+
+	//для корректной работы нужно передавать указатель на wg:
+	//for i := 0; i < 5; i++ {
+	//	wg.Add(1)
+	//	go func(wg *sync.WaitGroup, i int) {
+	//		fmt.Println(i)
+	//		wg.Done() //Done выполняется для копии wg из анонимной функции
+	//	}(&wg, i) //передаем копию wg
+	//}
 	wg.Wait()
 	fmt.Println("exit")
 }
--- 15/main.go
+++ 15/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+var (
+	first  = 3
+	second = 5
+)
+
+func main() {
+	fmt.Printf("First number = %v\nSecond number = %v\n\n", first, second)
+
+	first = first + second
+	second = first - second
+	first = first - second
+
+	fmt.Printf("After switch:\nFirst number = %v\nSecond number = %v\n", first, second)
 }
