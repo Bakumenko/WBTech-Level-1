9e3713d507f1e830c6a55cfe5280f256db686d77
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-29 12:49:34 +0300

    :zap:Complete 32.

--- 32/main.go
+++ 32/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func mySleepInSecond1(seconds int) {
+	<-time.After(time.Second * time.Duration(seconds))
+	fmt.Printf("%v seconds left\n", seconds)
+}
+
+func mySleepInSecond2(seconds int) {
+	start := time.Now()
+	sec := time.Duration(seconds)
+	for {
+		now := time.Now()
+		elapsed := now.Sub(start) / time.Second
+		if sec <= elapsed {
+			fmt.Printf("%v seconds left\n", seconds)
+			return
+		}
+	}
+}
+
+func mySleepInSecond3(seconds int) {
+	timer := time.NewTimer(time.Duration(seconds) * time.Second)
+	<-timer.C
+	fmt.Printf("%v seconds left\n", seconds)
+}
+
+func main() {
+	fmt.Println("Start")
+	fmt.Println("Start first sleep")
+	mySleepInSecond1(2)
+	fmt.Println("Start second sleep")
+	mySleepInSecond2(3)
+	fmt.Println("Start third sleep")
+	mySleepInSecond3(4)
+	fmt.Println("End")
 }
