af48a121580bdc7fc2f8a55974fa0073a0ebfaa0
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-29 17:52:22 +0300

    :zap:Complete 11

--- 11/main.go
+++ 11/main.go
@@ -3,16 +3,25 @@ package main
 import "fmt"
 
 var (
-	array11 = [5]int{2, 5, 3, 1, 4}
-	array21 = [6]int{5, 7, 6, 4, 8, 3}
+	slice11 = []int{2, 5, 3, 1, 4}
+	slice21 = []int{5, 7, 6, 4, 8, 3}
 )
 
-func main() {
-	for _, val1 := range array11 {
-		for _, val2 := range array21 {
+func crossingSlices1(s1 []int, s2 []int) []int {
+	var cross []int
+	for _, val1 := range s1 {
+		for _, val2 := range s2 {
 			if val1 == val2 {
-				fmt.Println(val1)
+				cross = append(cross, val1)
 			}
 		}
 	}
+	return cross
+}
+
+func main() {
+	cross := crossingSlices1(slice11, slice21)
+	fmt.Println("Slice1: ", slice11)
+	fmt.Println("Slice2: ", slice21)
+	fmt.Println("Crossing: ", cross)
 }
--- 11/main2.go
+++ 11/main2.go
@@ -1,16 +1,42 @@
 package main
 
-import (
-	"sort"
-)
+import "fmt"
 
 var (
-	array12 = []int{2, 5, 3, 1, 4}
-	array22 = []int{5, 7, 6, 4, 8, 3}
+	slice12 = []int{2, 5, 3, 1, 4}
+	slice22 = []int{5, 7, 6, 4, 8, 3}
 )
 
-func main() {
-	sort.Ints(array12)
-	sort.Ints(array22)
+func crossingSlices2(s1 []int, s2 []int) []int {
+	m := make(map[int]bool)
+	for _, val := range s1 {
+		if _, ok := m[val]; ok {
+			m[val] = true
+		} else {
+			m[val] = false
+		}
+	}
 
+	for _, val := range s2 {
+		if _, ok := m[val]; ok {
+			m[val] = true
+		} else {
+			m[val] = false
+		}
+	}
+
+	var cross []int
+	for key, value := range m {
+		if value {
+			cross = append(cross, key)
+		}
+	}
+	return cross
+}
+
+func main() {
+	cross := crossingSlices2(slice12, slice22)
+	fmt.Println("Slice1: ", slice12)
+	fmt.Println("Slice2: ", slice22)
+	fmt.Println("Crossing: ", cross)
 }
