7dde960aaa6ba2b851c3cd51df9ead1489b45aa4
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-29 12:59:17 +0300

    :zap:Complete 26.

--- 26/main.go
+++ 26/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+var s = "string with name Oleg, symbol 世界, ⌘ and русское слово"
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	fmt.Println("Started string = ", s)
+
+	rev := reverse(s)
+
+	fmt.Println("Finished string = ", rev)
 }
