dc2884b0de717141f91ed7fde78459615a5118b7
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-29 18:45:34 +0300

    :zap:Complete 10

--- 10/main.go
+++ 10/main.go
@@ -2,14 +2,23 @@ package main
 
 import "fmt"
 
-var array = [8]float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+var slice = []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5, -15, -17, -9, 1}
+
+func getTens(number float32) int {
+	if number >= 0 {
+		return int(number/10) * 10
+	} //else {
+	//return int(number/10 - 1) * 10
+	//}
+}
 
 func main() {
 
-	//group := make(map[int][]int)
+	group := make(map[int][]float32)
 
-	for temp := range array {
-
-		fmt.Println(temp)
+	for _, temp := range slice {
+		tens := getTens(temp)
+		group[tens] = append(group[tens], temp)
 	}
+	fmt.Println("Result: ", group)
 }
