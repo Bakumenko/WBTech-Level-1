4d6373d2dc025175134c02b38b2db79dcf028872
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-29 12:49:52 +0300

    :zap:Update 8.

--- 8/main.go
+++ 8/main.go
@@ -6,24 +6,24 @@ import (
 )
 
 var (
-	i      int   = 4
-	number int64 = 107
+	index  int   = 1
+	number int64 = 1
 )
 
 func main() {
-	if i > 0 && i < 8 {
+	if index > 0 && index < 8 {
 		b := make([]byte, 8)
 		binary.LittleEndian.PutUint64(b, uint64(number))
 
-		fmt.Printf("Old nubmer: %v\n", number)
-		fmt.Printf("Old nubmer in bytes: %v\n", b)
+		fmt.Printf("Old number: %v\n", number)
+		fmt.Printf("Old number in bytes: %v\n", b)
 
-		b[i] = 1
+		b[index] = 1
 		newNubmer := int64(binary.LittleEndian.Uint64(b))
 
-		fmt.Printf("New nubmer in bytes: %v\n", b)
+		fmt.Printf("New number in bytes: %v\n", b)
 		fmt.Printf("New number: %v\n", newNubmer)
 	} else {
-		fmt.Printf("%v is out of range 0..7", i)
+		fmt.Printf("%v is out of range 0..7", index)
 	}
 }
