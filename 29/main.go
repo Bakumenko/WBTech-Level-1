32e3d77f81d5b82adb3c41b61bed2c18c9bfe2d0
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-30 19:11:57 +0300

    :zap:Complete 29.

--- 29/main.go
+++ 29/main.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/big"
+)
 
+var (
+	a = int64(300000000 * 1000000)
+	b = int64(200000000 * 1000000)
+)
+
+type BigInt big.Int
+
+func NewBigInt(numb int64) *BigInt {
+	return (*BigInt)(big.NewInt(numb))
+}
+
+func (i *BigInt) Add(i2 *BigInt) *BigInt {
+	c := new(big.Int).Add((*big.Int)(i), (*big.Int)(i2))
+	return (*BigInt)(c)
+}
+
+func (i *BigInt) Sub(i2 *BigInt) *BigInt {
+	c := new(big.Int).Sub((*big.Int)(i), (*big.Int)(i2))
+	return (*BigInt)(c)
+}
+
+func (i *BigInt) Mul(i2 *BigInt) *BigInt {
+	c := new(big.Int).Mul((*big.Int)(i), (*big.Int)(i2))
+	return (*BigInt)(c)
+}
+
+func (i *BigInt) Div(i2 *BigInt) *BigInt {
+	c := new(big.Int).Div((*big.Int)(i), (*big.Int)(i2))
+	return (*BigInt)(c)
+}
+
+func (i *BigInt) String() string {
+	return (*big.Int)(i).String()
+}
+
+func main() {
+	n1 := NewBigInt(a)
+	n2 := NewBigInt(b)
+	fmt.Println("n1 = ", n1)
+	fmt.Println("n2 = ", n2)
+
+	add := n1.Add(n2)
+	sub := n1.Sub(n2)
+	mul := n1.Mul(n2)
+	div := n1.Div(n2)
+	fmt.Println("add = ", add)
+	fmt.Println("sub = ", sub)
+	fmt.Println("mul = ", mul)
+	fmt.Println("div = ", div)
+
+	fmt.Println("-----------")
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
+	fmt.Println("a * b = ", a*b)
 }
