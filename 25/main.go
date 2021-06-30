7133c46b49c5603daedf572baf29f20b3529e355
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-30 12:08:24 +0300

    :zap:Update 25.

--- 25/main.go
+++ 25/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+type Counter struct {
+	count int
+}
+
+func NewCounter() Counter {
+	return Counter{0}
+}
+
+func (c *Counter) add() {
+	c.count++
+}
+
+func (c *Counter) PrintCounter() {
+	fmt.Println("Counter: ", c.count)
+}
+
+func (c *Counter) Work(i <-chan interface{}) {
+	for value := range i {
+		c.add()
+		fmt.Printf("%v\n", value)
+	}
+}
+
+func main() {
+	c := make(chan interface{})
+	counter := NewCounter()
+	go counter.Work(c)
+
+	c <- 123
+	c <- "oleg"
+	c <- struct {
+		id   int
+		name string
+	}{1, "oleg"}
+
+	time.Sleep(1 * time.Second)
+
+	close(c)
+
+	counter.PrintCounter()
 }
