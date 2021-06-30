389ac18b82daca1cd6f911a100c912e79258486b
Oleg <74712214+obakumen@users.noreply.github.com>
2021-06-30 18:34:43 +0300

    :zap:Complete 1.

--- 1/main.go
+++ 1/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+type Human struct {
+	name string
+	age  int
+}
+
+type Action struct {
+	id  int
+	hum Human
+}
+
+func main() {
+	hum := Human{"Oleg", 24}
+	act := Action{1, hum}
+	fmt.Println(act)
 }
