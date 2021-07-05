c8c68f8bf835209ee41d79d21aa7197f0eab8dbd
Oleg <74712214+obakumen@users.noreply.github.com>
2021-07-05 18:49:27 +0300

    :zap:Update 35

---
+++ 35/additionalMain3.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	slice3 = []interface{}{
+		12321,
+		12322,
+		"123321",
+		124321,
+		"asa",
+		"oleg",
+	}
+
+	resultMap = map[interface{}]bool{}
+)
+
+func isPalindromeUniversalConcurrency(targer interface{}, wg *sync.WaitGroup, mu *sync.Mutex) {
+	str := fmt.Sprintf("%v", targer)
+	for i := 0; i < len(str)/2; i++ {
+		if str[i] != str[len(str)-1-i] {
+			mu.Lock()
+			resultMap[targer] = false
+			mu.Unlock()
+			wg.Done()
+			return
+		}
+	}
+	resultMap[targer] = true
+	wg.Done()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	for _, val := range slice3 {
+		wg.Add(1)
+		go isPalindromeUniversalConcurrency(val, &wg, &mu)
+	}
+
+	wg.Wait()
+	for key, val := range resultMap {
+		if val {
+			fmt.Printf("%v is palindrome\n", key)
+		} else {
+			fmt.Printf("%v is not palindrome\n", key)
+		}
+	}
+}
